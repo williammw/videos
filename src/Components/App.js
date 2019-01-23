0ced0bb9ff54f03a0cd3c663cd86770de9f4c39d
Man Wai Wong <william.manwai@gmail.com>
2019-01-24 00:58:26 +0800

    async await action pair

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
 
 class App extends React.Component {
 
-    onTermSubmit = term => {
-        console.log(term);
+    onTermSubmit = async term => {
+        //console.log(term);
+        const response = await youtube.get('search', {
+            params: {
+                q:term
+            }
+        })
+        console.log(response)
     }
 
     render(){
