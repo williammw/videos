4bb2317a292fb408734e2d5448ec0e7303a1e8a0
wmwwong <wmwwong@legco.gov.hk>
2019-01-24 12:17:21 +0800

    beta 0.0.1

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
+
+    state = {
+        videos: [],
+        selectedVideo: null
+    };
+
+    componentDidMount(){
+        this.onTermSubmit('make me feel your love')
+    }
 
     onTermSubmit = async term => {
         //console.log(term);
@@ -11,13 +22,33 @@ class App extends React.Component {
                 q:term
             }
         })
-        console.log(response)
+        //console.log(response)
+        
+        this.setState({
+            videos: response.data.items,
+            selectedVideo : response.data.items[0]
+        })
+    }
+
+    onVideoSelect = (video) => {
+        this.setState({selectedVideo:video})
     }
 
     render(){
         return(
-            <div className="ui container">
+            <div className="ui container">                               
                 <SearchBar onFormSubmit={this.onTermSubmit} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />               
+                        </div>
+                    </div>
+                </div>
+                
             </div>
         );
     }
